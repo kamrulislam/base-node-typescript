0ad3e2a4808aa255a83c82ed12c1a9b41841e8c1
Md Kamrul Islam <kamruli@gmail.com>
2019-08-23 19:32:12 +1000

    Added my version of promisify using typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-const greeter = (person: string) => {
-    return "Hello, " + person;
+import { promisify } from './promisify';
+
+const greeter = (person: string, callback?: any) => {
+    const greet = "Hello, " + person;
+    if (callback) {
+        callback(null, greet);
+    }
+    
+    return greet;
 }
 
 let user = "Kamrul Islam";
 
 console.log(greeter(user));
+
+console.log(greeter(user, (greet: any) => {
+    console.log(`callback ${greet}`);
+}));
+
+const asyncCall = async () => {
+    await promisify(greeter)(user).then((result: any) => console.log(`promisify ${result}`));
+} 
+
+asyncCall();
+
+
---
+++ src/promisify.ts
@@ -0,0 +1,27 @@
+// inspired by https://medium.com/trabe/understanding-nodes-promisify-and-callbackify-d2b04efde0e0
+
+const CUSTOM = Symbol("promisify.custom");
+
+type CallbackFunction = (...args: any[]) => Promise<any>;
+type CallbackFunctionObject = {(...args: any[]): void; [CUSTOM]?: CallbackFunction}
+
+const promisify = (fn : CallbackFunctionObject): CallbackFunction => {
+    if (fn[CUSTOM]) {
+        return fn[CUSTOM];
+    }    
+
+    return (...args: any[]): Promise<any> => new Promise((resolve, reject) => {
+        fn(...args, (error: any, value: any) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(value);
+            }
+        });
+
+    });
+}
+
+export {
+    promisify
+}

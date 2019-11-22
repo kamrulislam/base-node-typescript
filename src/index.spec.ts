bdc8524cb4883a66f57b06189b3d85dfcbf0e903
Md Kamrul Islam <kamruli@gmail.com>
2019-11-22 18:58:08 +1100

    fixed tslint issues

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,3 @@
-test('hello', () => {
+test('hello', (): void => {
   expect('hello').toBe('hello');
-}); 
+});
--- src/promisify.ts
+++ src/promisify.ts
@@ -1,14 +1,13 @@
 // inspired by https://medium.com/trabe/understanding-nodes-promisify-and-callbackify-d2b04efde0e0
-
-const CUSTOM = Symbol("promisify.custom");
+const CUSTOM = Symbol('promisify.custom');
 
 type CallbackFunction = (...args: any[]) => Promise<any>;
-type CallbackFunctionObject = {(...args: any[]): void; [CUSTOM]?: CallbackFunction}
+interface CallbackFunctionObject {(...args: any[]): void; [CUSTOM]?: CallbackFunction;}
 
 const promisify = (fn : CallbackFunctionObject): CallbackFunction => {
     if (fn[CUSTOM]) {
         return fn[CUSTOM];
-    }    
+    }
 
     return (...args: any[]): Promise<any> => new Promise((resolve, reject) => {
         fn(...args, (error: any, value: any) => {
@@ -20,9 +19,9 @@ const promisify = (fn : CallbackFunctionObject): CallbackFunction => {
         });
 
     });
-}
+};
 
 export {
     promisify,
     CUSTOM
-}
+};
